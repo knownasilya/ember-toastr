760986ad0b48b608b2f9f6ad336a38c98bb8f569
Greg Coladarci <greg@peek.com>
2015-09-28 16:35:22 -0700

    override success/error/etc to use to string

--- addon/initializers/toastr.js
+++ addon/initializers/toastr.js
@@ -1,8 +1,23 @@
+import Ember from 'ember';
+
 export function initialize(container, application, options) {
   var injectAs = options.injectAs;
-  toastr.options = options.toastrOptions;
+  window.toastr.options = options.toastrOptions;
 
-  application.register('notify:main', toastr, { instantiate: false, singleton: true });
+  application.register('notify:main', Object.assign({}, toastr, Ember.Object.create({
+    success(msg) {
+      window.toastr.success(msg.toString());
+    },
+    info(msg) {
+      window.toastr.info(msg.toString());
+    },
+    warning(msg) {
+      window.toastr.warning(msg.toString());
+    },
+    error(msg) {
+      window.toastr.error(msg.toString());
+    }
+  })), { instantiate: false, singleton: true });
   application.inject('route', injectAs, 'notify:main');
   application.inject('controller', injectAs, 'notify:main');
   application.inject('component', injectAs, 'notify:main');
