98d31973f63461d0c89e43bedfc3beb4c106cf79
Greg Coladarci <greg@peek.com>
2015-09-29 07:02:41 -0700

    share logic and allow title

--- addon/initializers/toastr.js
+++ addon/initializers/toastr.js
@@ -1,22 +1,20 @@
 import Ember from 'ember';
 
+var proxyGenerator = function(name){
+  return function(msg = '', title = '') {
+    window.toastr[name](msg.toString(), title.toString())
+  };
+};
+
 export function initialize(container, application, options) {
   var injectAs = options.injectAs;
   window.toastr.options = options.toastrOptions;
 
   application.register('notify:main', Object.assign({}, toastr, Ember.Object.create({
-    success(msg) {
-      window.toastr.success(msg.toString());
-    },
-    info(msg) {
-      window.toastr.info(msg.toString());
-    },
-    warning(msg) {
-      window.toastr.warning(msg.toString());
-    },
-    error(msg) {
-      window.toastr.error(msg.toString());
-    }
+    success: proxyGenerator('success'),
+    info: proxyGenerator('info'),
+    warning: proxyGenerator('warning'),
+    error: proxyGenerator('error')
   })), { instantiate: false, singleton: true });
   application.inject('route', injectAs, 'notify:main');
   application.inject('controller', injectAs, 'notify:main');
